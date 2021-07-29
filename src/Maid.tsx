1b428b00ecb73506a969429054d7fd562ea12f2f
slify08 <joshxfi.dev@gmail.com>
2021-07-29 18:58:45 +0800

    update 1.0.3

--- src/Maid.tsx
+++ src/Maid.tsx
@@ -1,35 +1,51 @@
 /** @jsx jsx */
 /** @jsxFrag */
-import React from 'react'
-import { css, jsx, Global } from '@emotion/react'
+import React from "react";
+import { css, jsx, Global } from "@emotion/react";
 
 const Maid: React.FC = () => {
-    return (
-        <>
-            <Global styles={css`
+  return (
+    <>
+      <Global
+        styles={css`
+          * {
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+            -webkit-tap-highlight-color: transparent;
+            font-family: inherit;
+          }
 
-                * {
-                    padding: 0;
-                    margin: 0;
-                    box-sizing: border-box;
-                    -webkit-tap-highlight-color: transparent;
-                }
+          body {
+            line-height: 1;
+          }
 
-                a {
-                    text-decoration: none;
-                }
+          a {
+            text-decoration: none;
+          }
 
-                ul {
-                    list-style: none;
-                }
+          ol,
+          ul {
+            list-style: none;
+          }
 
-                button, input, textarea {
-                    outline: none;
-                }
+          input,
+          textarea {
+            outline: none;
+          }
 
-            `} />
-        </>
-    )
-}
+          textarea {
+            resize: none;
+          }
 
-export default Maid
+          table {
+            border-collapse: collapse;
+            border-spacing: 0;
+          }
+        `}
+      />
+    </>
+  );
+};
+
+export default Maid;
